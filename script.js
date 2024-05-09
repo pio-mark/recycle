7ea4d13375166e3d861bcf148f5540d06c7ae3ba
Piotr Markiewicz <252940@student.pwr.edu.pl>
2024-05-09 22:37:00 +0200

    Poprawki

--- script.js
+++ script.js
@@ -30,13 +30,13 @@ function includeHeader() {
 const items = [
 'W 2021 roku niemal połowa (49,6%) wszystkich odpadów komunalnych w UE poddawana była recyklingowi lub kompostowaniu',
 'Z 35 plastikowych butelek można stworzyć jedną bluzę polarową',
-'W recyklingu przodują Szwedzi, doskonale sobie radzą z przetwarzaniemodpadów. Dziś jedynie 3% odpadów w tym skandynawskim kraju trafia na wysypiska, reszta jest efektywnie wykorzystywana',
+'W recyklingu przodują Szwedzi, doskonale sobie radzą z przetwarzaniem odpadów. Dziś jedynie 3% odpadów w tym skandynawskim kraju trafia na wysypiska, reszta jest efektywnie wykorzystywana',
 'Pierwszy przypadek recyklingu odnotowano w 1776 roku',
 'Recykling jednej tony papieru może uratować około 17 drzew, 7 000 galonów wody, 3 metry sześcienne miejsca na wysypisku śmieci i 4 000 kWh energii elektrycznej',
 'Szkło jest jednym z najbardziej trwałych materiałów i może być recyklowane wielokrotnie bez utraty jakości',
 'Opony samochodowe są jednym z najtrudniejszych do utylizacji odpadów. Jednak mogą być one poddane recyklingowi. Stare opony mogą być przetwarzane na nowe opony, materiały izolacyjne, nawierzchnie dróg, gumowe kruszywo do podłoża i inne produkty',
-'W jednym z japońskich miast, Jokohamie, można spotkać strażników segregacji,którzy zajmują się pilnowaniem przestrzegania zasad postępowania z odpadami',
-'W mieście Kanton w Chinach są nakładane ogromne kary pieniężne jeśli człowiek w ciągu dnia wyprodukuje więcej 1kg śmieci dziennie',
+'W jednym z japońskich miast, Jokohamie, można spotkać strażników segregacji, którzy zajmują się pilnowaniem przestrzegania zasad postępowania z odpadami',
+'W mieście Kanton w Chinach są nakładane ogromne kary pieniężne, jeśli człowiek w ciągu dnia wyprodukuje więcej niż 1kg śmieci dziennie',
 'W Indiach w mieście Ghazipur znajduje się wysypisko śmieci o wielkości 40 boisk piłkarskich'
 ];
 
@@ -54,7 +54,7 @@ function pickRandomItem() {
         document.getElementById('result').textContent = `${selectedItem}!`;
     } else {
         // If user input is invalid, display an error message
-        document.getElementById('result').textContent = 'Prosze podaj numer od 1 do 10.';
+        document.getElementById('result').textContent = 'Proszę podaj numer od 1 do 10.';
     }
 }
 
@@ -62,5 +62,5 @@ function pickRandomItem() {
 window.onload = function() {
     const randomIndex = Math.floor(Math.random() * 10); // Generate a random number between 0 and 9
     const randomItem = items[randomIndex];
-    document.getElementById('result').textContent = ` ${randomItem}`;
+    document.getElementById('result').textContent = ` ${randomItem}!`;
 };
